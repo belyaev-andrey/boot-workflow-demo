083dbad0ccb0e03101e5b2bb682d6fc540123520
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-01-03 15:30:50 +0100

    Initial commit

---
+++ src/main/java/org/example/bootworkflowdemo/BootWorkflowDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootworkflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWorkflowDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootWorkflowDemoApplication.class, args);
+    }
+
+}
